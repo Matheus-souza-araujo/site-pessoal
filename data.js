8abc3512153d8ad657b247348ed79f3c5011d760
Matheus-souza-araujo <matheus1mrs@gmail.com>
2020-09-24 20:20:10 -0300

    Site finalizado no bootcamp

---
+++ data.js
@@ -0,0 +1,46 @@
+module.exports = [
+    {
+        id:"uBTW96xL_fQ",
+        title:"Recipe - inglês",
+        durantion: "5 min",
+        price:"Free",
+        featured: true
+    },
+
+    {
+        id:"uBTW96xL_fQ",
+        title:"Recipe - inglês",
+        durantion: "5 min",
+        price:"Free"
+    },
+
+    {
+        id:"uBTW96xL_fQ",
+        title:"Recipe - inglês",
+        durantion: "5 min",
+        price:"Free"
+    },
+
+    {
+        id:"uBTW96xL_fQ",
+        title:"Recipe - inglês",
+        durantion: "5 min",
+        price:"Free",
+        featured: true
+    },
+
+    {
+        id:"uBTW96xL_fQ",
+        title:"Recipe - inglês",
+        durantion: "5 min",
+        price:"Free"
+    },
+
+    {
+        id:"uBTW96xL_fQ",
+        title:"Recipe - inglês",
+        durantion: "5 min",
+        price:"Free",
+        featured: true
+    },
+]
